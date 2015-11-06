171b7d9c8873640abd74fe5f3fea600bc67e3fad
VirtualBoxImages.com <adminuser@adminuser-VirtualBox.(none)>
2015-11-07 01:03:57 +0200

    first commmit

---
+++ mongotest.py
@@ -0,0 +1,25 @@
+__author__ = 'adminuser'
+
+import mongo_markov
+
+
+
+connect=mongo_markov.MarkovChain()
+connect.insert('power_on','power_off',1234,'battery')
+connect.insert('power_off','power_on',1234,'battery')
+connect.insert('power_on','power_on',1234,'battery')
+connect.insert('power_off','power_off',1234,'battery')
+connect.insert('power_on','power_off',1234,'battery')
+connect.insert('power_off','power_on',1234,'battery')
+#print(connect.get_state(1234,'battery'))
+#print(connect.get_prediction('power_on',1234,'battery'))
+
+
+print"Running the state and prediction :"
+
+for x in range(0,10):
+    print (connect.get_state_and_prediction(1234,'battery'))
+
+
+print(connect.client)
+print(connect.collection)
